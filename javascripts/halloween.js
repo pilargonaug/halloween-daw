a82b450324197f2f90fcfe5d4c22077636111043
pilargonaug <37416625+pilargonaug@users.noreply.github.com>
2019-10-28 10:53:48 +0100

    Primer intento de halloween

---
+++ javascripts/halloween.js
@@ -0,0 +1,13 @@
+         /* DECLARACIÓN DE FUNCIONES */
+         function mostrar(elemento) {
+            document.getElementById(elemento).style.visibility="visible";
+         }
+         function ocultar(elemento) {
+            document.getElementById(elemento).style.visibility="hidden";
+         }
+         function sonar(sonido){
+              document.getElementById(sonido).play();
+         }
+         function pausar(sonido){
+              document.getElementById(sonido).pause();
+         }
